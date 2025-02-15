d8037243ee1a1d6dc08e07dbd17f6ff9e676105f
Bryan Thompson <bryant@amazon.com>
2025-02-15 14:47:26 +0000

    Adds float and double ordering checks.  Some things are misordered (negative vs positive). Checkpoint before refactoring to fix that.

--- test/test_key_encode_decode.cpp
+++ test/test_key_encode_decode.cpp
@@ -47,16 +47,22 @@ constexpr auto INITIAL_CAPACITY = my_key_encoder::get_initial_capacity();
 
 // Test helper verifies that [ekey1] < [ekey2].
 template <typename T>
-void do_encode_decode_order_test(const T ekey1, const T ekey2) {
+void do_encode_decode_lt_test(const T ekey1, const T ekey2) {
   unodb::key_encoder enc1{};
   unodb::key_encoder enc2{};  // separate decoder (backed by different span).
   const auto ikey1 = enc1.encode(ekey1).get_key_view();  // into encoder buf!
   const auto ikey2 = enc2.encode(ekey2).get_key_view();  // into encoder buf!
-  EXPECT_EQ(compare(ikey1, ikey1), 0);
-  EXPECT_EQ(compare(ikey2, ikey2), 0);
-  EXPECT_NE(compare(ikey1, ikey2), 0);
-  EXPECT_LT(compare(ikey1, ikey2), 0);
-  EXPECT_GT(compare(ikey2, ikey1), 0);
+  EXPECT_TRUE(compare(ikey1, ikey1) == 0);               // compare w/ self
+  EXPECT_TRUE(compare(ikey2, ikey2) == 0);               // compare w/ self
+  EXPECT_NE(ekey1, ekey2);                               // not the same ekey.
+  EXPECT_TRUE(compare(ikey1, ikey2) != 0);               // not the same ikey.
+  EXPECT_TRUE(compare(ikey1, ikey2) < 0)                 // 1 LT 2
+      << ": key1(" << ekey1 << ")"
+      << ", key2(" << ekey2 << ")";
+  EXPECT_TRUE(compare(ikey2, ikey1) > 0)  // 2 > 1
+      << ": key1(" << ekey1 << ")"
+      << ", key2(" << ekey2 << ")";
+  // Verify that we can round trip both values.
   unodb::key_decoder dec1{ikey1};
   unodb::key_decoder dec2{ikey2};
   T akey1;
@@ -158,10 +164,10 @@ TEST(ARTKeyEncodeDecodeTest, UInt8C00010) {
   do_encode_decode_test(std::numeric_limits<T>::min() + one);
   do_encode_decode_test(std::numeric_limits<T>::max() - one);
   // check lexicographic ordering for std::uint8_t pairs.
-  do_encode_decode_order_test(static_cast<T>(0x01ULL), static_cast<T>(0x09ULL));
-  do_encode_decode_order_test(static_cast<T>(0), static_cast<T>(1));
-  do_encode_decode_order_test(static_cast<T>(0x7FULL), static_cast<T>(0x80ULL));
-  do_encode_decode_order_test(static_cast<T>(0xFEULL), static_cast<T>(~0ULL));
+  do_encode_decode_lt_test(static_cast<T>(0x01ULL), static_cast<T>(0x09ULL));
+  do_encode_decode_lt_test(static_cast<T>(0), static_cast<T>(1));
+  do_encode_decode_lt_test(static_cast<T>(0x7FULL), static_cast<T>(0x80ULL));
+  do_encode_decode_lt_test(static_cast<T>(0xFEULL), static_cast<T>(~0ULL));
 }
 
 TEST(ARTKeyEncodeDecodeTest, Int8C00010) {
@@ -180,14 +186,12 @@ TEST(ARTKeyEncodeDecodeTest, Int8C00010) {
   do_encode_decode_test(std::numeric_limits<T>::min() + one);
   do_encode_decode_test(std::numeric_limits<T>::max() - one);
   // check lexicographic ordering for std::uint8_t pairs.
-  do_encode_decode_order_test(static_cast<T>(0), static_cast<T>(1));
-  do_encode_decode_order_test(static_cast<T>(5), static_cast<T>(7));
-  do_encode_decode_order_test(
-      std::numeric_limits<T>::min(),
-      static_cast<T>(std::numeric_limits<T>::min() + one));
-  do_encode_decode_order_test(
-      static_cast<T>(std::numeric_limits<T>::max() - one),
-      std::numeric_limits<T>::max());
+  do_encode_decode_lt_test(static_cast<T>(0), static_cast<T>(1));
+  do_encode_decode_lt_test(static_cast<T>(5), static_cast<T>(7));
+  do_encode_decode_lt_test(std::numeric_limits<T>::min(),
+                           static_cast<T>(std::numeric_limits<T>::min() + one));
+  do_encode_decode_lt_test(static_cast<T>(std::numeric_limits<T>::max() - one),
+                           std::numeric_limits<T>::max());
 }
 
 TEST(ARTKeyEncodeDecodeTest, UInt16C00010) {
@@ -206,12 +210,12 @@ TEST(ARTKeyEncodeDecodeTest, UInt16C00010) {
   do_encode_decode_test(std::numeric_limits<T>::min() + one);
   do_encode_decode_test(std::numeric_limits<T>::max() - one);
   // check lexicographic ordering for std::uint16_t pairs.
-  do_encode_decode_order_test(static_cast<T>(0x0102ULL),
-                              static_cast<T>(0x090AULL));
-  do_encode_decode_order_test(static_cast<T>(0), static_cast<T>(1));
-  do_encode_decode_order_test(static_cast<T>(0x7FFFULL),
-                              static_cast<T>(0x8000ULL));
-  do_encode_decode_order_test(static_cast<T>(0xFFFEULL), static_cast<T>(~0ULL));
+  do_encode_decode_lt_test(static_cast<T>(0x0102ULL),
+                           static_cast<T>(0x090AULL));
+  do_encode_decode_lt_test(static_cast<T>(0), static_cast<T>(1));
+  do_encode_decode_lt_test(static_cast<T>(0x7FFFULL),
+                           static_cast<T>(0x8000ULL));
+  do_encode_decode_lt_test(static_cast<T>(0xFFFEULL), static_cast<T>(~0ULL));
 }
 
 TEST(ARTKeyEncodeDecodeTest, Int16C00010) {
@@ -230,14 +234,12 @@ TEST(ARTKeyEncodeDecodeTest, Int16C00010) {
   do_encode_decode_test(std::numeric_limits<T>::min() + one);
   do_encode_decode_test(std::numeric_limits<T>::max() - one);
   // check lexicographic ordering for std::uint16_t pairs.
-  do_encode_decode_order_test(static_cast<T>(0), static_cast<T>(1));
-  do_encode_decode_order_test(static_cast<T>(5), static_cast<T>(7));
-  do_encode_decode_order_test(
-      std::numeric_limits<T>::min(),
-      static_cast<T>(std::numeric_limits<T>::min() + one));
-  do_encode_decode_order_test(
-      static_cast<T>(std::numeric_limits<T>::max() - one),
-      std::numeric_limits<T>::max());
+  do_encode_decode_lt_test(static_cast<T>(0), static_cast<T>(1));
+  do_encode_decode_lt_test(static_cast<T>(5), static_cast<T>(7));
+  do_encode_decode_lt_test(std::numeric_limits<T>::min(),
+                           static_cast<T>(std::numeric_limits<T>::min() + one));
+  do_encode_decode_lt_test(static_cast<T>(std::numeric_limits<T>::max() - one),
+                           std::numeric_limits<T>::max());
 }
 
 TEST(ARTKeyEncodeDecodeTest, Uint32C00010) {
@@ -257,13 +259,13 @@ TEST(ARTKeyEncodeDecodeTest, Uint32C00010) {
   do_encode_decode_test(std::numeric_limits<T>::min() + one);
   do_encode_decode_test(std::numeric_limits<T>::max() - one);
   // check lexicographic ordering for std::uint32_t pairs.
-  do_encode_decode_order_test(static_cast<T>(0x01020304ULL),
-                              static_cast<T>(0x090A0B0CULL));
-  do_encode_decode_order_test(static_cast<T>(0), static_cast<T>(1));
-  do_encode_decode_order_test(static_cast<T>(0x7FFFFFFFULL),
-                              static_cast<T>(0x80000000ULL));
-  do_encode_decode_order_test(static_cast<T>(0xFFFFFFFEULL),
-                              static_cast<T>(~0ULL));
+  do_encode_decode_lt_test(static_cast<T>(0x01020304ULL),
+                           static_cast<T>(0x090A0B0CULL));
+  do_encode_decode_lt_test(static_cast<T>(0), static_cast<T>(1));
+  do_encode_decode_lt_test(static_cast<T>(0x7FFFFFFFULL),
+                           static_cast<T>(0x80000000ULL));
+  do_encode_decode_lt_test(static_cast<T>(0xFFFFFFFEULL),
+                           static_cast<T>(~0ULL));
 }
 
 TEST(ARTKeyEncodeDecodeTest, Int32C00010) {
@@ -287,12 +289,12 @@ TEST(ARTKeyEncodeDecodeTest, Int32C00010) {
   do_encode_decode_test(std::numeric_limits<T>::max());
   do_encode_decode_test(std::numeric_limits<T>::max() - one);
   // check lexicographic ordering for std::uint32_t pairs.
-  do_encode_decode_order_test(0, 1);
-  do_encode_decode_order_test(5, 7);
-  do_encode_decode_order_test(std::numeric_limits<T>::min(),
-                              std::numeric_limits<T>::min() + one);
-  do_encode_decode_order_test(std::numeric_limits<T>::max() - one,
-                              std::numeric_limits<T>::max());
+  do_encode_decode_lt_test(0, 1);
+  do_encode_decode_lt_test(5, 7);
+  do_encode_decode_lt_test(std::numeric_limits<T>::min(),
+                           std::numeric_limits<T>::min() + one);
+  do_encode_decode_lt_test(std::numeric_limits<T>::max() - one,
+                           std::numeric_limits<T>::max());
 }
 
 TEST(ARTKeyEncodeDecodeTest, UInt64C00010) {
@@ -314,10 +316,10 @@ TEST(ARTKeyEncodeDecodeTest, UInt64C00010) {
   do_encode_decode_test(std::numeric_limits<T>::min() + one);
   do_encode_decode_test(std::numeric_limits<T>::max() - one);
   // check lexicographic ordering for std::uint64_t pairs.
-  do_encode_decode_order_test<T>(0x0102030405060708ULL, 0x090A0B0C0D0F1011ULL);
-  do_encode_decode_order_test(static_cast<T>(0), static_cast<T>(1));
-  do_encode_decode_order_test<T>(0x7FFFFFFFFFFFFFFFULL, 0x8000000000000000ULL);
-  do_encode_decode_order_test<T>(0xFFFFFFFFFFFFFFFEULL, static_cast<T>(~0ULL));
+  do_encode_decode_lt_test<T>(0x0102030405060708ULL, 0x090A0B0C0D0F1011ULL);
+  do_encode_decode_lt_test(static_cast<T>(0), static_cast<T>(1));
+  do_encode_decode_lt_test<T>(0x7FFFFFFFFFFFFFFFULL, 0x8000000000000000ULL);
+  do_encode_decode_lt_test<T>(0xFFFFFFFFFFFFFFFEULL, static_cast<T>(~0ULL));
 }
 
 TEST(ARTKeyEncodeDecodeTest, Int64C00010) {
@@ -339,12 +341,12 @@ TEST(ARTKeyEncodeDecodeTest, Int64C00010) {
   do_encode_decode_test(std::numeric_limits<T>::min() + one);
   do_encode_decode_test(std::numeric_limits<T>::max() - one);
   // check lexicographic ordering for std::uint64_t pairs.
-  do_encode_decode_order_test(static_cast<T>(0), static_cast<T>(1));
-  do_encode_decode_order_test(static_cast<T>(5), static_cast<T>(7));
-  do_encode_decode_order_test(std::numeric_limits<T>::min(),
-                              std::numeric_limits<T>::min() + one);
-  do_encode_decode_order_test(std::numeric_limits<T>::max() - one,
-                              std::numeric_limits<T>::max());
+  do_encode_decode_lt_test(static_cast<T>(0), static_cast<T>(1));
+  do_encode_decode_lt_test(static_cast<T>(5), static_cast<T>(7));
+  do_encode_decode_lt_test(std::numeric_limits<T>::min(),
+                           std::numeric_limits<T>::min() + one);
+  do_encode_decode_lt_test(std::numeric_limits<T>::max() - one,
+                           std::numeric_limits<T>::max());
 }
 
 //
@@ -486,6 +488,23 @@ TEST(ARTKeyEncodeDecodeTest, FloatC0006NumericNaN) {
   do_encode_decode_float_test(std::nanf("-100.1"));
 }
 
+/// Verify the ordering over various floating point pairs.
+TEST(ARTKeyEncodeDecodeTest, FloatC0007Order) {
+  using F = float;
+  constexpr auto minf = std::numeric_limits<F>::min();
+  constexpr auto maxf = std::numeric_limits<F>::max();
+  constexpr auto inf = std::numeric_limits<F>::infinity();
+  constexpr auto ninf = -std::numeric_limits<F>::infinity();
+  constexpr auto lowest = std::numeric_limits<F>::lowest();
+  do_encode_decode_lt_test(-10.01F, -1.01F);
+  do_encode_decode_lt_test(-1.F, 0.0F);
+  do_encode_decode_lt_test(0.F, 1.0F);
+  do_encode_decode_lt_test(1.01F, 10.01F);
+  do_encode_decode_lt_test(ninf, lowest);
+  do_encode_decode_lt_test(0.F, minf);
+  do_encode_decode_lt_test(maxf, inf);
+}
+
 /// Can be used for anything (handles NaN as a special case).
 void do_encode_decode_double_test(const double expected) {
   using U = std::uint64_t;
@@ -578,6 +597,24 @@ TEST(ARTKeyEncodeDecodeTest, DoubleC0006NumericNaN) {
   do_encode_decode_double_test(std::nan("1"));
   do_encode_decode_double_test(std::nan("100.1"));
   do_encode_decode_double_test(std::nan("-100.1"));
+}
+
+/// Verify the ordering over various double precision floating point
+/// pairs.
+TEST(ARTKeyEncodeDecodeTest, DoubleC0007Order) {
+  using F = double;
+  constexpr auto minf = std::numeric_limits<F>::min();
+  constexpr auto maxf = std::numeric_limits<F>::max();
+  constexpr auto inf = std::numeric_limits<F>::infinity();
+  constexpr auto ninf = -std::numeric_limits<F>::infinity();
+  constexpr auto lowest = std::numeric_limits<F>::lowest();
+  do_encode_decode_lt_test(-10.01, -1.01);
+  do_encode_decode_lt_test(-1., 0.0);
+  do_encode_decode_lt_test(0., 1.0);
+  do_encode_decode_lt_test(1.01, 10.01);
+  do_encode_decode_lt_test(ninf, lowest);
+  do_encode_decode_lt_test(0., minf);
+  do_encode_decode_lt_test(maxf, inf);
 }
 
 //
