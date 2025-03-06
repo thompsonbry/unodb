cbb704fd7d9d940d155ac8deb9f49bca2960d235
Bryan Thompson <bryant@amazon.com>
2025-03-06 16:55:21 +0000

    WIP - more tests.  all new tests fail, but I have not touched the mutation code paths yet.

--- test/test_art_key_view.cpp
+++ test/test_art_key_view.cpp
@@ -76,15 +76,63 @@ TYPED_TEST(ARTKeyViewCorrectnessTest, EncodedTextKeys) {
   verifier.check_present_values();  // checks keys and key ordering.
 }
 
-/// This test inserts a single key which is longer than the
-/// unodb::detail::key_prefix plus the `key_byte`.  In this case, the
-/// key needs to be unfolded as a sequence of inodes until the entire
-/// key is absorbed.  Thus, the tree depth for this single key is
-/// `key.size_bytes() / 8`.
-///
-// TODO(thompsonbry) no root leaf.  Write more unit tests to probe
-// more cases.
-TYPED_TEST(ARTKeyViewCorrectnessTest, SingleKeyUnfoldedByINodePath) {
+// A series of test which insert a single key which is longer than the
+// unodb::detail::key_prefix plus the `key_byte`.  The key needs to be
+// unfolded as a sequence of inodes until the entire key is absorbed.
+
+TYPED_TEST(ARTKeyViewCorrectnessTest, Single9ByteKeyUnfoldedByINodePath) {
+  unodb::test::tree_verifier<TypeParam> verifier;
+  // key is an 8-byte prefix of the key we insert.
+  constexpr auto a0 = std::array<std::byte, 8>{
+      std::byte{0x00}, std::byte{0x01}, std::byte{0x02}, std::byte{0x03},
+      std::byte{0x04}, std::byte{0x05}, std::byte{0x06}, std::byte{0x07}};
+  // key is 9-bytes long, so it splits the root inode.
+  constexpr auto a1 = std::array<std::byte, 9>{
+      std::byte{0x00}, std::byte{0x01}, std::byte{0x02},
+      std::byte{0x03}, std::byte{0x04}, std::byte{0x05},
+      std::byte{0x06}, std::byte{0x07}, std::byte{0x10}};
+  unodb::key_view k0{a0};
+  unodb::key_view k1{a1};
+  verifier.insert(k1, unodb::test::test_values[2]);
+
+  verifier.assert_root_not_leaf();
+  verifier.check_present_values();
+  verifier.check_absent_keys({k0});
+
+#ifdef UNODB_DETAIL_WITH_STATS
+  verifier.assert_node_counts({0, 2, 0, 0, 0});  // one leaf, two I4
+  verifier.assert_growing_inodes({2, 0, 0, 0});  // two I4
+#endif                                           // UNODB_DETAIL_WITH_STATS
+}
+
+TYPED_TEST(ARTKeyViewCorrectnessTest, Single15ByteKeyUnfoldedByINodePath) {
+  unodb::test::tree_verifier<TypeParam> verifier;
+  // key is an 8-byte prefix of the key we insert.
+  constexpr auto a0 = std::array<std::byte, 8>{
+      std::byte{0x00}, std::byte{0x01}, std::byte{0x02}, std::byte{0x03},
+      std::byte{0x04}, std::byte{0x05}, std::byte{0x06}, std::byte{0x07}};
+  // key is 15-bytes long, so we split the root inode but the key does
+  // not require another layer.
+  constexpr auto a1 = std::array<std::byte, 15>{
+      std::byte{0x00}, std::byte{0x01}, std::byte{0x02}, std::byte{0x03},
+      std::byte{0x04}, std::byte{0x05}, std::byte{0x06}, std::byte{0x07},
+      std::byte{0x10}, std::byte{0x11}, std::byte{0x12}, std::byte{0x13},
+      std::byte{0x14}, std::byte{0x15}, std::byte{0x16}};
+  unodb::key_view k0{a0};
+  unodb::key_view k1{a1};
+  verifier.insert(k1, unodb::test::test_values[2]);
+
+  verifier.assert_root_not_leaf();
+  verifier.check_present_values();
+  verifier.check_absent_keys({k0});
+
+#ifdef UNODB_DETAIL_WITH_STATS
+  verifier.assert_node_counts({1, 2, 0, 0, 0});  // one leaf, two I4
+  verifier.assert_growing_inodes({2, 0, 0, 0});  // two I4
+#endif                                           // UNODB_DETAIL_WITH_STATS
+}
+
+TYPED_TEST(ARTKeyViewCorrectnessTest, Single16ByteKeyUnfoldedByINodePath) {
   unodb::test::tree_verifier<TypeParam> verifier;
   // key is an 8-byte prefix of the key we insert.
   constexpr auto a0 = std::array<std::byte, 8>{
@@ -105,8 +153,43 @@ TYPED_TEST(ARTKeyViewCorrectnessTest, SingleKeyUnfoldedByINodePath) {
   verifier.check_absent_keys({k0});
 
 #ifdef UNODB_DETAIL_WITH_STATS
-  verifier.assert_node_counts({0, 2, 0, 0, 0});  // two I4
+  verifier.assert_node_counts({0, 2, 0, 0, 0});  // one leaf, two I4
   verifier.assert_growing_inodes({2, 0, 0, 0});  // two I4
+#endif                                           // UNODB_DETAIL_WITH_STATS
+}
+
+TYPED_TEST(ARTKeyViewCorrectnessTest, Single17ByteKeyUnfoldedByINodePath) {
+  unodb::test::tree_verifier<TypeParam> verifier;
+  // key is an 8-byte prefix of the key we insert.
+  constexpr auto a0 = std::array<std::byte, 8>{
+      std::byte{0x00}, std::byte{0x01}, std::byte{0x02}, std::byte{0x03},
+      std::byte{0x04}, std::byte{0x05}, std::byte{0x06}, std::byte{0x07}};
+  // key is an 16-byte prefix of the key we insert.
+  constexpr auto a1 = std::array<std::byte, 16>{
+      std::byte{0x00}, std::byte{0x01}, std::byte{0x02}, std::byte{0x03},
+      std::byte{0x04}, std::byte{0x05}, std::byte{0x06}, std::byte{0x07},
+      std::byte{0x10}, std::byte{0x11}, std::byte{0x12}, std::byte{0x13},
+      std::byte{0x14}, std::byte{0x15}, std::byte{0x16}, std::byte{0x17},
+  };
+  // key is 17-bytes long, so it splits the 2nd inode.
+  constexpr auto a2 = std::array<std::byte, 17>{
+      std::byte{0x00}, std::byte{0x01}, std::byte{0x02}, std::byte{0x03},
+      std::byte{0x04}, std::byte{0x05}, std::byte{0x06}, std::byte{0x07},
+      std::byte{0x10}, std::byte{0x11}, std::byte{0x12}, std::byte{0x13},
+      std::byte{0x14}, std::byte{0x15}, std::byte{0x16}, std::byte{0x17},
+      std::byte{0x20}};
+  unodb::key_view k0{a0};
+  unodb::key_view k1{a1};
+  unodb::key_view k2{a2};
+  verifier.insert(k2, unodb::test::test_values[2]);
+
+  verifier.assert_root_not_leaf();
+  verifier.check_present_values();
+  verifier.check_absent_keys({k0, k1});
+
+#ifdef UNODB_DETAIL_WITH_STATS
+  verifier.assert_node_counts({1, 3, 0, 0, 0});  // one leaf, two I4s.
+  verifier.assert_growing_inodes({3, 0, 0, 0});  // two I4
 #endif                                           // UNODB_DETAIL_WITH_STATS
 }
 
